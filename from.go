a0f9699c5aee02ec178c8968db4460025557d17a
Joseph Edwards Van Riper III <vanriper.trey@gmail.com>
2025-06-03 14:41:43 -0400

    Update from.go
    CutPrefix doesn't exist for strings.
    
    Signed-off-by: Joseph Edwards Van Riper III <vanriper.trey@gmail.com>

--- from.go
+++ from.go
@@ -14,7 +14,7 @@ var TimeFormat string = "Mon Jan  2 15:04:05 2006"
 // It helpfully translates the date/time to a time.Time.  A mailer might use
 // this information in some way, if needed.
 func ParseFrom(from string) (addr string, date time.Time, moreinfo string, err error) {
-	data, _ := strings.CutPrefix(from, "From ")
+	data, _ := strings.TrimPrefix(from, "From ")
 	addr, remainder, _ := strings.Cut(data, " ")
 	if len(remainder) >= len(TimeFormat) {
 		date, err = time.Parse(TimeFormat, strings.TrimSpace(remainder[:len(TimeFormat)]))
